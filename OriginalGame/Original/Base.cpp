e2169cfb07dbb2bbd4a60a22d3989b9a8c2d7152
SilVerC77 <silverc777@hotmail.com>
2022-02-02 21:36:47 +0900

    Added Base Class

---
+++ OriginalGame/Original/Base.cpp
@@ -0,0 +1,26 @@
+#include "Base.h"
+#include "CPicture.h"
+
+Base::Base()
+	:m_pBody(nullptr)
+	, m_pHitBox(nullptr)
+	, m_Pos(0.f, 0.f)
+	, m_Size(0.f, 0.f)
+{
+
+}
+
+Base::~Base()
+{
+
+}
+
+void Base::BeginPlay()
+{
+
+}
+
+void Base::Tick(const float _DeltaTime)
+{
+
+}
---
+++ OriginalGame/Original/Base.h
@@ -0,0 +1,44 @@
+﻿/**
+* @file CPicture.h
+* @brief テクセル付きのピクチャー・・クラス
+* @author 織戸　喜隆
+* @date 日付（2017.02.14）
+*/
+#pragma once
+#include "stdafx.h"
+
+/*! @class CPicture
+@brief  ポリゴン・クラス
+*/
+class Base
+{
+	class CPicture* m_pBody;
+	CPicture* m_pHitBox;
+	XMFLOAT2 m_Pos;
+	XMFLOAT2 m_Size;
+public:
+	Base();					//コンストラクタ
+	virtual ~Base();		//デストラクタ
+
+	/********************************
+	* Method:Public
+	*********************************/
+	virtual void BeginPlay();
+	virtual void Tick(const float _DeltaTime);
+
+private:
+	/********************************
+	* Method:Private
+	*********************************/
+
+protected:
+public:
+	/********************************
+	* Getter:Public
+	*********************************/
+	CPicture* GetBody()const { return m_pBody; }
+	CPicture* GetHitBox()const { return m_pHitBox; }
+	XMFLOAT2 GetPos()const { return m_Pos; }
+	XMFLOAT2 GetSize()const { return m_Size; }
+};
+
